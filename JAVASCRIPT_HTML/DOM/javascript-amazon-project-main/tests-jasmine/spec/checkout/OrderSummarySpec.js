449d822b78849659c0a2eeaca71e52c9261e4ea8
PedroCarvalho1500 <pedro_alves@gec.inatel.br>
2024-11-03 12:40:44 -0300

    MOD: BeforeEach and AfterEach implemented.

--- JAVASCRIPT_HTML/DOM/javascript-amazon-project-main/tests-jasmine/spec/checkout/OrderSummarySpec.js
+++ JAVASCRIPT_HTML/DOM/javascript-amazon-project-main/tests-jasmine/spec/checkout/OrderSummarySpec.js
@@ -4,40 +4,9 @@ import { addProductToCart,cart,cartIconAdded,saveToStorage,updateCartProduct,rem
 
 describe('Test Set: organizeCart', () => 
 {
-    it('Displays the cart', () => 
-    {
-        document.querySelector('.js-test-container').innerHTML = `
-        <div class="order-summary"> </div>
-        `;
-
-        const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
-        const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d"
-        spyOn(localStorage, 'getItem').and.callFake(() => 
-        {
-                return JSON.stringify(
-                    [
-                        {
-                            productId: productId1,
-                            quantity: 1,
-                            deliveryOptionId: '1'
-                        },
-                        {
-                            productId: productId2,
-                            quantity: 3,
-                            deliveryOptionId: '2'
-                        }
-                    ]);
-        });
-            
-        loadFromStorage();
-        organizeCart();
-        expect(document.querySelectorAll('.cart-item-container').length).toEqual(2);
-        expect(document.querySelector(`.quantity-product-${productId1}`).innerText).toContain(`Quantity: 1`);
-        expect(document.querySelector(`.quantity-product-${productId2}`).innerText).toContain(`Quantity: 3`);
-        document.querySelector('.js-test-container').innerHTML = ``;
-    });
-
-    it('Removes a product from the cart', () => 
+    const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+    const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+    beforeEach(() => 
     {
         document.querySelector('.js-test-container').innerHTML = `
         <div class="order-summary"> </div>
@@ -45,27 +14,37 @@ describe('Test Set: organizeCart', () =>
         <div class="payment-summary"></div>
         `;
 
-        const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
-        const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d"
         spyOn(localStorage, 'getItem').and.callFake(() => 
-        {
-                return JSON.stringify(
-                    [
-                        {
-                            productId: productId1,
-                            quantity: 1,
-                            deliveryOptionId: '1'
-                        },
-                        {
-                            productId: productId2,
-                            quantity: 3,
-                            deliveryOptionId: '2'
-                        }
-                    ]);
-        });
+            {
+                    return JSON.stringify(
+                        [
+                            {
+                                productId: productId1,
+                                quantity: 1,
+                                deliveryOptionId: '1'
+                            },
+                            {
+                                productId: productId2,
+                                quantity: 3,
+                                deliveryOptionId: '2'
+                            }
+                        ]);
+            });
+
+            loadFromStorage();
+            organizeCart();
+    })
+
+    it('Displays the cart', () => 
+    {
+        expect(document.querySelectorAll('.cart-item-container').length).toEqual(2);
+        expect(document.querySelector(`.quantity-product-${productId1}`).innerText).toContain(`Quantity: 1`);
+        expect(document.querySelector(`.quantity-product-${productId2}`).innerText).toContain(`Quantity: 3`);
+    });
+
+    it('Removes a product from the cart', () => 
+    {
         spyOn(localStorage, 'setItem');
-        loadFromStorage();
-        organizeCart();
         document.querySelectorAll(`.delete-quantity-link`)[0].click();
         expect(document.querySelectorAll('.cart-item-container').length).toEqual(1);
         expect(document.querySelector(`.quantity-product-${productId2}`).innerText).toContain(`Quantity: 3`);
@@ -73,7 +52,11 @@ describe('Test Set: organizeCart', () =>
         expect(document.querySelector(`.quantity-product-${productId2}`)).not.toEqual(null);
         expect(cart.length).toEqual(1);
         expect(cart[0].productId).toEqual(productId2);
-        document.querySelector('.js-test-container').innerHTML = ``;
+        
 
+    });
+
+    afterEach(() => {
+        document.querySelector('.js-test-container').innerHTML = ``;
     })
 })
